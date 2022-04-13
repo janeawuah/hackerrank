14617b7909049352224797797d7e09198594a2b5
Jane Awuah <janeawuah@gmail.com>
2022-04-13 07:28:35 +0000

    added divisble sum pairs function

---
+++ src/main/com.hackerrank/HackFunctions.java
@@ -0,0 +1,50 @@
+package main.com.hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HackFunctions {
+
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        List<Integer> brokenRecords = new ArrayList<>();
+        int minimumScore = scores.get(0);
+        int maximumScore = scores.get(0);
+        int minCounter = 0;
+        int maxCounter = 0;
+
+        for (int i = 1; i < scores.size(); i++) {
+            if (scores.get(i) > maximumScore) {
+                maxCounter += 1;
+                maximumScore = scores.get(i);
+            }
+            if (scores.get(i) < minimumScore) {
+                minCounter += 1;
+                minimumScore = scores.get(i);
+            }
+        }
+        brokenRecords.add(maxCounter);
+        brokenRecords.add(minCounter);
+
+        return brokenRecords;
+    }
+
+
+    //day 3
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+        // Write your code here
+        int numOfDivisiblePairs = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (((ar.get(i) + ar.get(j)) % k) == 0) {
+                    System.out.println(ar.get(i) + ar.get(j));
+                    numOfDivisiblePairs = numOfDivisiblePairs + 1;
+                }
+            }
+        }
+        return numOfDivisiblePairs;
+    }
+
+
+}
--- src/main/com.hackerrank/Main.java
+++ src/main/com.hackerrank/Main.java
@@ -1,9 +1,13 @@
 package main.com.hackerrank;
 
+
 public class Main {
+
 
     public static void main(String[] args)
     {
+        HackFunctions hackfuns = new HackFunctions();
+
 
     }
 }
