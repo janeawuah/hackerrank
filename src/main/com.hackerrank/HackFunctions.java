a765c0ff13b352c78e9d3644acbdccc30f675ce9
Jane Awuah <janeawuah@gmail.com>
2022-04-27 06:56:12 +0000

    added a fun function to print stairs

--- src/main/com.hackerrank/HackFunctions.java
+++ src/main/com.hackerrank/HackFunctions.java
@@ -125,6 +125,23 @@ public class HackFunctions {
     }
 
 
+    public static void staircase(int n) {
+        // Write your code here
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+
+            for(int k=0;k<i;k++){
+                System.out.print("#");
+            }
+
+            System.out.println("");
+        }
+
+    }
+
+
 
 
 
