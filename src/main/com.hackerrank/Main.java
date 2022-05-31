84b0b487ffe517b38943de0bb17ea34b134ac17b
Jane Awuah <janeawuah@gmail.com>
2022-05-31 20:41:04 +0000

    formatting outputs in java main

--- src/main/com.hackerrank/Main.java
+++ src/main/com.hackerrank/Main.java
@@ -10,17 +10,30 @@ public class Main {
     {
 //        HackFunctions hackfuns = new HackFunctions();
 
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        double d  = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
-        // Write your code here.
+//        Scanner scan = new Scanner(System.in);
+//        int i = scan.nextInt();
+//        double d  = scan.nextDouble();
+//        scan.nextLine();
+//        String s = scan.nextLine();
+//        // Write your code here.
+//
+//        System.out.println("String: " + s);
+//        System.out.println("Double: " + d);
+//        System.out.println("Int: " + i);
+//        scan.close();
 
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
-        scan.close();
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+            System.out.printf("%-15s%03d%n", s1,x);
+        }
+        System.out.println("================================");
+
 
 
     }
